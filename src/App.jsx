cee093a5957c08f4a2c0b7dcbf7a6af8b1c330bd
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-09-28 21:46:41 +0530

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-black">
+      <img
+        className="w-1/2 h-1/2 object-contain"
+        src="https://media4.giphy.com/media/4JEGvm7EV3KOsYNAvZ/200w.gif?cid=6c09b952ku8f1k710ykh2y3o23ddballierqecwi6ot6wa1f&ep=v1_gifs_search&rid=200w.gif&ct=g"
+        alt="comming soon"
+      />
+    </div>
+  );
+}
+
+export default App;
