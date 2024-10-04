3b70fcd374fb526759b76c1f94f3f155169bf729
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-10-04 09:06:42 +0530

    bug fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,19 +9,21 @@ import Inventory from "./Inventory";
 export default function Home() {
   return (
     <div className="">
-    <Tabs aria-label="Default tabs" variant="underline" >
-      <Tabs.Item active title="Billing" icon={SiCashapp}>
-        <Billing />
-      </Tabs.Item>
-      <Tabs.Item active title="Inventory" icon={AiOutlineStock}>
-        <Inventory />
-      </Tabs.Item>
-      <Tabs.Item disabled title="Admin Panel" icon={GrUserAdmin}>
-        Disabled content
-      </Tabs.Item>
-    </Tabs>
-    <div className="absolute top-2 right-4">
-        <h1 className="font-bold text-2xl text-cyan-500 cursor-default">Nethmina <span className="text-sky-600">Mobile</span> </h1>
+      <Tabs aria-label="Default tabs" variant="underline" className="bg-white">
+        <Tabs.Item active title="Billing" icon={SiCashapp}>
+          <Billing />
+        </Tabs.Item>
+        <Tabs.Item active title="Inventory" icon={AiOutlineStock}>
+          <Inventory />
+        </Tabs.Item>
+        <Tabs.Item disabled title="Admin Panel" icon={GrUserAdmin}>
+          Disabled content
+        </Tabs.Item>
+      </Tabs>
+      <div className="absolute top-2 right-4">
+        <h1 className="font-bold text-2xl text-cyan-500 cursor-default">
+          Nethmina <span className="text-sky-600">Mobile</span>{" "}
+        </h1>
       </div>
     </div>
   );
--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Inventory() {
-  return (
-    <div>Inventory</div>
-  )
+  return <div className="bg-slate-100">Inventory</div>;
 }
