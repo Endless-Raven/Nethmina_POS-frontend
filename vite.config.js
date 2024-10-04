8995c9def247391655d5635b93407a3a3a193ec8
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-10-04 13:55:39 +0530

    connect db to get category

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        secure: false,
-      },
+  plugins: [react()],
+  define: {
+    'process.env': {
+      API_BASE_URL: 'http://localhost:3000',
     },
   },
-  plugins: [react()],
 })
