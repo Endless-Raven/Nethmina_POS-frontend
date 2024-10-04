90198b0cd04b6e16953ec94e88edcbff9181dcd6
Chathu-Karunarathne <karunarathnechathu001@gmail.com>
2024-10-04 13:35:31 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Billing from "./pages/Billing";
 import Inventory from "./pages/Inventory";
+import DailyReport from "./pages/DailyReport";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route element={<Billing />} path="/" />
         <Route element={<Inventory/>} path="/inv" />
+        <Route element={<DailyReport/>} path="/drepot"/>
       </Routes>
     </BrowserRouter>
   );
