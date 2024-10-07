cca0e57f60a576c7736470f35122dbb13a7a3e15
Chathu-Karunarathne <karunarathnechathu001@gmail.com>
2024-10-07 13:31:48 +0530

    create not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+
 
 export default function NotFoundPage() {
   return (
-    <div>NotFoundPage...........404</div>
-  )
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-center p-8 border rounded-lg bg-white shadow-lg">
+        <h1 className="text-6xl font-bold text-red-500">404</h1>
+        <p className="mt-4 text-xl text-gray-700">Page Not Found</p>
+        <p className="mt-2 text-gray-500">
+          Oops, this file does not exist. Try look for something else.
+        </p>
+        <a
+        href="/"
+          className="mt-6 cursor-pointer inline-block px-6 py-3 text-white bg-blue-500 rounded hover:bg-blue-600"
+        >
+          Go Back Home
+        </a>
+      </div>
+    </div>
+  );
 }
