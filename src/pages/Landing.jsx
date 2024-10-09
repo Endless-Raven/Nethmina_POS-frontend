4f9d10a2614509ea10eff2e5392d29824b14c807
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-10-09 12:29:03 +0530

    fix bg

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,7 +7,7 @@ export default function Landing() {
 
   return (
     <div className="flex flex-col justify-center items-center gap-6 text-white bg-cover bg-center bg-black h-screen relative]">
-      <img className="w-full h-screen object-cover absolute opacity-50 z-0 " src="./public/landingBg.jpg" alt="d" />
+      <img className="w-full h-screen object-cover absolute opacity-50 z-0 " src="/landingBg.jpg" alt="d" />
       <h1 className="ml-72 font-bold text-7xl text-cyan-500 cursor-default text-center z-20">
         Nethmina <span className="text-sky-600">Mobile</span>{" "}
       </h1>
