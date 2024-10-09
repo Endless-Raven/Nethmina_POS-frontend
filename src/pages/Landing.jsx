50ddb2909a370652d5ac3618c2160c7accad75ca
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-10-09 12:52:47 +0530

    fix bg not showing issue in landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "flowbite-react";
+import bg from "../images/LandingBg.jpg";
 
 export default function Landing() {
   const navigate = useNavigate();
 
   return (
     <div className="flex flex-col justify-center items-center gap-6 text-white bg-cover bg-center bg-black h-screen relative]">
-      <img className="w-full h-screen object-cover absolute opacity-50 z-0 " src="/landingBg.jpg" alt="d" />
+      <img className="w-full h-screen object-cover absolute opacity-50 z-0 " src={bg} alt="d" />
       <h1 className="ml-72 font-bold text-7xl text-cyan-500 cursor-default text-center z-20">
         Nethmina <span className="text-sky-600">Mobile</span>{" "}
       </h1>
@@ -23,6 +24,5 @@ export default function Landing() {
         </Button>
       </div>
     </div>
-    
   );
 }
