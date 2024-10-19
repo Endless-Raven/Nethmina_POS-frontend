7733993428c60ed12535a885a0c73acf07d86968
Chathu-Karunarathne <karunarathnechathu001@gmail.com>
2024-10-19 22:59:19 +0530

    create status table

--- src/screens/StatusAdminInventory.jsx
+++ src/screens/StatusAdminInventory.jsx
@@ -7,21 +7,21 @@ function StatusAdminInventory() {
   ];
 
   const confirmedTransfers = [
-    { category: 'Phone', brand: 'Apple', productName: 'iPhone 14', qty: 3 },
+    { category: 'Phone', brand: 'Apple', productName: 'iPhone 12', qty: 3 },
     { category: 'Accessory', brand: 'Anker', productName: 'Charger', qty: 15 },
   ];
 
   return (
-    <div className="p-4 ">
+    <div className="p-4  ">
       {/* Pending Transfer Section */}
-      <div className="flex justify-center">
+      <div className="flex justify-center gap-4">
         <div className="w-1/2">
-          <h2 className="text-lg font-bold mb-2">Pending Transfer</h2>
-          <div className="border p-2 mb-4 flex justify-between">
+          <h2 className="text-lg font-bold mb-2 text-green-500 ">Pending Transfer</h2>
+          <div className="p-2 mb-4 flex justify-start gap-4 bg-green-100 text-green-400">
             <strong>From:</strong> Kurunegala
             <strong>To:</strong> Kandy
           </div>
-          <table className="min-w-full border-collapse">
+          <table className="min-w-full border-collapse bg-green-100">
             <thead>
               <tr>
                 <th className="border px-4 py-2">Category</th>
@@ -44,13 +44,13 @@ function StatusAdminInventory() {
         </div>
 
         {/* Confirmed Transfer Section */}
-        <div className="w-1/2">
-          <h2 className="text-lg font-bold mb-2">Confirmed Transfer</h2>
-          <div className="border p-2 mb-4">
-            <p><strong>From:</strong>  Kurunegala</p>
-            <p><strong>To:</strong> Polonnaruwa</p>
+         <div className="w-1/2">
+          <h2 className="text-lg font-bold mb-2 text-blue-500">Confirmed Transfer</h2>
+          <div className=" p-2 mb-4 flex justify-start gap-4 bg-blue-100 text-blue-400">
+            <strong>From:</strong>  Kurunegala
+            <strong>To:</strong> Polonnaruwa
           </div>
-          <table className="min-w-full border-collapse">
+          <table className="min-w-full border-collapse bg-blue-100">
             <thead>
               <tr>
                 <th className="border px-4 py-2">Category</th>
