f9148d6c77f592d91a467302d90a8bfefdf269e2
Chathu-Karunarathne <karunarathnechathu001@gmail.com>
2024-10-24 15:32:30 +0530

    create manager inventory ui

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,14 +1,90 @@
-import React, { useState } from "react";
-import { Button } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { Button, Checkbox, Label, Select, TextInput } from "flowbite-react";
+import { CiSearch } from "react-icons/ci";
 import UpcommingStockInv from "../components/UpcommingStockInv";
 import RequestProduct from "../components/RequestProduct";
+import { InventoryTable } from "../components/InventoryTable";
 
 const Inventory = () => {
+  const productTypesAndCategories = [
+    {
+      product_types: "mobile phone",
+      brand_names: ["samsung", "apple", "huawei", "oneplus", "google"],
+    },
+    {
+      product_types: "mobile accessory",
+      brand_names: ["anker", "belkin", "mi", "spigen", "jbl"],
+    },
+    {
+      product_types: "laptop",
+      brand_names: ["dell", "hp", "asus", "lenovo", "acer"],
+    },
+    {
+      product_types: "tablet",
+      brand_names: ["apple", "samsung", "lenovo", "microsoft", "huawei"],
+    },
+    {
+      product_types: "smartwatch",
+      brand_names: ["fitbit", "apple", "samsung", "garmin", "fossil"],
+    },
+  ];
+  
+  const [selectedType, setSelectedType] = useState("");
+  
+  useEffect(() => {
+    if (productTypesAndCategories && productTypesAndCategories.length > 0) {
+      setSelectedType(productTypesAndCategories[0].product_types);
+    }
+  }, []);
+
   const [openModalUpcomming, setOpenModalUpcomming] = useState(false);
   const [openModalRequest, setOpenModalRequest] = useState(false);
 
+  console.log(selectedType);
   return (
     <div className="p-6 bg-slate-100 min-h-[88vh]">
+      {/* Search Bar and  Selectors */}
+      <div className="mb-4">
+        <div className="flex items-center gap-4">
+          {/* Search Bar */}
+          <TextInput
+            icon={CiSearch}
+            placeholder="Search Item"
+            className="w-1/3"
+          />
+
+          {/* Category Selector */}
+          <Select value={selectedType} className="w-1/4" onChange={(e)=>{setSelectedType(e.target.value)}}>
+            {productTypesAndCategories &&
+              productTypesAndCategories.length > 0 &&
+              productTypesAndCategories.map((product, index) => (
+                <option value={product.product_types} key={index}>
+                  {product.product_types}
+                </option>
+              ))}
+          </Select>
+
+          {/* Brand Selector */}
+          <Select className="w-1/4">
+            <option value="all">All Brands</option>
+            <option value="brand1">Brand 1</option>
+            <option value="brand2">Brand 2</option>
+          </Select>
+        </div>
+      </div>
+
+      {/* Checkbox and Paragraph */}
+      <div className="mb-6 flex items-center gap-2">
+        <Checkbox id="low-stock" />
+        <Label htmlFor="low-stock" className="cursor-pointer">
+          Show only low stock items
+        </Label>
+      </div>
+
+      {/* Stock table */}
+      <InventoryTable />
+
+      {/* Bottom Buttons */}
       <div className="flex justify-between fixed bottom-2 left-0 w-full px-6">
         <Button
           gradientDuoTone="purpleToBlue"
@@ -29,6 +105,8 @@ const Inventory = () => {
           </Button>
         </div>
       </div>
+
+      {/* Modals */}
       <UpcommingStockInv
         show={openModalUpcomming}
         close={() => setOpenModalUpcomming(false)}
