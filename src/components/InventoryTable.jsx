feb5a363f5a32e62543bbc2723b675992f527a91
Chathu-Karunarathne <karunarathnechathu001@gmail.com>
2024-10-25 15:07:02 +0530

    create manager inventory table

--- src/components/InventoryTable.jsx
+++ src/components/InventoryTable.jsx
@@ -8,6 +8,72 @@ import {
 } from "flowbite-react";
 
 export function InventoryTable() {
+  const productList = [
+    {
+      product_id: 1,
+      product_name: "Samsung Galaxy S23",
+      brand_name: "Samsung",
+      product_type: "Mobile Phone",
+      product_stock: 30,
+      product_wholesale_price: 30000,
+      product_price: 50000,
+    },
+    {
+      product_id: 2,
+      product_name: "Samsung Galaxy S23",
+      brand_name: "Samsung",
+      product_type: "Mobile Phone",
+      product_stock: 30,
+      product_wholesale_price: 30000,
+      product_price: 50000,
+    },
+    {
+      product_id: 3,
+      product_name: "iPhone 14",
+      brand_name: "Apple",
+      product_type: "Mobile Phone",
+      product_stock: 50,
+      product_wholesale_price: 45000,
+      product_price: 75000,
+    },
+    {
+      product_id: 4,
+      product_name: "Google Pixel 7",
+      brand_name: "Google",
+      product_type: "Mobile Phone",
+      product_stock: 20,
+      product_wholesale_price: 35000,
+      product_price: 60000,
+    },
+    {
+      product_id: 5,
+      product_name: "OnePlus 11",
+      brand_name: "OnePlus",
+      product_type: "Mobile Phone",
+      product_stock: 40,
+      product_wholesale_price: 28000,
+      product_price: 55000,
+    },
+    {
+      product_id: 6,
+      product_name: "Sony Xperia 5 IV",
+      brand_name: "Sony",
+      product_type: "Mobile Phone",
+      product_stock: 25,
+      product_wholesale_price: 32000,
+      product_price: 52000,
+    },
+    {
+      product_id: 7,
+      product_name: "Xiaomi Mi 13",
+      brand_name: "Xiaomi",
+      product_type: "Mobile Phone",
+      product_stock: 60,
+      product_wholesale_price: 25000,
+      product_price: 48000,
+    },
+  ];
+
   return (
     <div className="overflow-x-auto">
       <Table striped>
@@ -19,80 +85,25 @@ export function InventoryTable() {
           <TableHeadCell>Qty</TableHeadCell>
           <TableHeadCell>Wholesale Price</TableHeadCell>
           <TableHeadCell>Retail Price</TableHeadCell>
-          <TableHeadCell>Store</TableHeadCell>
+          
         </TableHead>
         <TableBody className="divide-y">
-          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              1
-            </TableCell>
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {'Apple iPhone 14 Pro"'}
-            </TableCell>
-            <TableCell>Apple</TableCell>
-            <TableCell>Phone</TableCell>
-            <TableCell>15</TableCell>
-            <TableCell>$950</TableCell>
-            <TableCell>$1200</TableCell>
-            <TableCell>Store 1</TableCell>
-          </TableRow>
-          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              2
-            </TableCell>
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Microsoft Surface Pro
-            </TableCell>
-            <TableCell>White</TableCell>
-            <TableCell>Laptop PC</TableCell>
-            <TableCell>15</TableCell>
-            <TableCell>$950</TableCell>
-            <TableCell>$1200</TableCell>
-            <TableCell>Store 1</TableCell>
-          </TableRow>
-          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              3
-            </TableCell>
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Magic Mouse 2
-            </TableCell>
-            <TableCell>Black</TableCell>
-            <TableCell>Accessories</TableCell>
-            <TableCell>$99</TableCell>
-            <TableCell>$999</TableCell>
-            <TableCell>$999</TableCell>
-            <TableCell>$999</TableCell>
-            
-          </TableRow>
-          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              4
-            </TableCell>
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Google Pixel Phone
-            </TableCell>
-            <TableCell>Gray</TableCell>
-            <TableCell>Phone</TableCell>
-            <TableCell>$799</TableCell>
-            <TableCell>$999</TableCell>
-            <TableCell>$999</TableCell>
-            <TableCell>$999</TableCell>
-          </TableRow>
-          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              5
-            </TableCell>
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Apple Watch 5
-            </TableCell>
-            <TableCell>Red</TableCell>
-            <TableCell>Wearables</TableCell>
-            <TableCell>$999</TableCell>
-            <TableCell>$999</TableCell>
-            <TableCell>$999</TableCell>
-            <TableCell>$999</TableCell>
-          </TableRow>
+          {productList.map((product, index) => (
+            <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {index +1}
+              </TableCell>
+              <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {product.product_name}
+              </TableCell>
+              <TableCell>{product.brand_name}</TableCell>
+              <TableCell>{product.product_type}</TableCell>
+              <TableCell>{product.product_stock}</TableCell>
+              <TableCell>{product.product_wholesale_price}</TableCell>
+              <TableCell>{product.product_price}</TableCell>
+              
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </div>
