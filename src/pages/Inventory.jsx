c2722b2785a8cb730f628d98806a39b8728f77ae
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-10-29 16:34:03 +0530

    complete admin inventory page

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,88 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { Button, Checkbox, Label, Select, TextInput } from "flowbite-react";
+import { Button, Checkbox, Label, Select, Spinner, TextInput } from "flowbite-react";
 import { CiSearch } from "react-icons/ci";
 import UpcommingStockInv from "../components/UpcommingStockInv";
 import RequestProduct from "../components/RequestProduct";
 import { InventoryTable } from "../components/InventoryTable";
 import InventoryPendingRequest from "../components/InventoryPendingRequest";
+import axios from "axios";
+import { useSelector } from "react-redux";
+const API_BASE_URL = process.env.API_BASE_URL;
 
 const Inventory = () => {
-  const [productTypes, setProductTypes] = useState([
-    "Mobile Accessory",
-    "Mobile Phone",
-    "Wearable",
-    "Electronics",
-    "mobile_phone",
-  ]);
-  const [brands, setBrands] = useState(["Sony", "Generic"]);
-  const products = [
-    {
-      product_id: 3,
-      product_name: "Samsung Galaxy A21s",
-      product_price: "50000.00",
-      warranty_period: "1",
-      created_at: "2024-10-10T04:21:06.000Z",
-      updated_at: "2024-10-27T09:58:57.000Z",
-      product_stock: "7.00",
-      product_type: "Mobile Phone",
-      brand_name: "Samsung",
-      product_model: "A21s",
-      product_wholesale_price: "0.00",
-      imei_numbers: "123456789012345,987654321098765",
-      imei_number: [
-        "182734659900876",
-        "888776954345695",
-        "445623456886954",
-        "754456758844321",
-      ],
-    },
-    {
-      product_id: 16,
-      product_name: "Samsung s5",
-      product_price: "7000.00",
-      warranty_period: null,
-      created_at: "2024-10-11T13:17:52.000Z",
-      updated_at: "2024-10-11T13:18:21.000Z",
-      product_stock: "400.00",
-      product_type: "Mobile Phone",
-      brand_name: "Samsung",
-      product_model: "s5",
-      product_wholesale_price: "2500.00",
-      imei_numbers: null,
-      imei_number: ["111225469887532", "232233223456765", "122234500978654"],
-    },
-    {
-      product_id: 37,
-      product_name: "test 16",
-      product_price: "100.00",
-      warranty_period: null,
-      created_at: "2024-10-16T11:09:05.000Z",
-      updated_at: "2024-10-26T08:35:48.000Z",
-      product_stock: "12.00",
-      product_type: "Mobile Phone",
-      brand_name: "Samsung",
-      product_model: "",
-      product_wholesale_price: "11.00",
-      imei_numbers: null,
-      imei_number: [],
-    },
-    {
-      product_id: 39,
-      product_name: "test 18",
-      product_price: "1000.00",
-      warranty_period: "",
-      created_at: "2024-10-18T04:39:42.000Z",
-      updated_at: "2024-10-18T04:39:42.000Z",
-      product_stock: "1000.00",
-      product_type: "Mobile Phone",
-      brand_name: "Samsung",
-      product_model: "A21s",
-      product_wholesale_price: "1800.00",
-      imei_numbers: null,
-      imei_number: [],
-    },
-  ];
-
+  const userData = useSelector((state) => state.user.data);
+  const [productTypes, setProductTypes] = useState([]);
+  const [brands, setBrands] = useState([]);
+  const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [selectedType, setSelectedType] = useState("");
   const [selectedBrand, setSelectedBrand] = useState("");
@@ -90,15 +21,82 @@ const Inventory = () => {
   const [openModalRequest, setOpenModalRequest] = useState(false);
   const [openModalPending, setOpenModalPending] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function getAllTypes() {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await axios.get(
+        `${API_BASE_URL}/product/getProductTypes/get`
+      );
+      setProductTypes(response.data);
+      setSelectedType(response.data[0]);
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function getBrandsByType() {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await axios.get(
+        `${API_BASE_URL}/product/brands/byproducttype`,
+        {
+          params: {
+            product_type: selectedType,
+          },
+        }
+      );
+      setBrands(response.data);
+      setSelectedBrand(response.data[0]);
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function getProductsByTypeBrand() {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await axios.get(
+        `${API_BASE_URL}/product/models/bybrand`,
+        {
+          params: {
+            product_type: selectedType,
+            brand_name: selectedBrand,
+          },
+        }
+      );
+      setProducts(response.data);
+      setFilteredProducts(response.data);
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   useEffect(() => {
-    if (productTypes && productTypes.length > 0) {
-      setSelectedType(productTypes[0]);
-    }
-    if (products && products.length > 0) {
-      setFilteredProducts(products);
-    }
+    getAllTypes();
   }, []);
+
+  useEffect(() => {
+    getBrandsByType();
+  }, [selectedType]);
+
+  useEffect(() => {
+    getProductsByTypeBrand();
+  }, [selectedType, selectedBrand]);
 
   useEffect(() => {
     setFilteredProducts(
@@ -109,112 +107,131 @@ const Inventory = () => {
   }, [searchQuery]);
 
   return (
-    <div className="p-6 bg-slate-100 min-h-[88vh]">
-      {/* Search Bar and Selectors */}
-      <div className="mb-4">
-        <div className="flex items-center gap-4">
-          {/* Search Bar */}
-          <TextInput
-            icon={CiSearch}
-            placeholder="Search Item"
-            className="w-1/3"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
+    <div className="relative min-h-[88vh]">
+      {loading ? (
+        <div className="min-h-[50vh] flex justify-center items-center">
+          <div className="flex items-center gap-4">
+            <Spinner size="lg" />
+            <span className="pl-3 text-slate-400 text-3xl">Loading...</span>
+          </div>
+        </div>
+      ) : error ? (
+        <div className="min-h-[50vh] flex justify-center items-center">
+          <div className="flex items-center gap-4">
+            <span className="pl-3 text-red-400 text-3xl">
+              Something went wrong
+            </span>
+          </div>
+        </div>
+      ) : (
+        <div className="p-6 bg-slate-100 min-h-[88vh]">
+          {/* Search Bar and Selectors */}
+          <div className="mb-4">
+            <div className="flex items-center gap-4">
+              {/* Search Bar */}
+              <TextInput
+                icon={CiSearch}
+                placeholder="Search Item"
+                className="w-1/3"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+
+              {/* Category Selector */}
+              <Select
+                value={selectedType}
+                className="w-1/4"
+                onChange={(e) => {
+                  setSelectedType(e.target.value);
+                }}
+              >
+                {productTypes.map((product, index) => (
+                  <option value={product} key={index}>
+                    {product}
+                  </option>
+                ))}
+              </Select>
+
+              {/* Brand Selector */}
+              <Select
+                value={selectedBrand}
+                className="w-1/4"
+                onChange={(e) => {
+                  setSelectedBrand(e.target.value);
+                }}
+              >
+                {brands.map((brand, index) => (
+                  <option key={index} value={brand}>
+                    {brand}
+                  </option>
+                ))}
+              </Select>
+            </div>
+          </div>
+
+          {/* Checkbox and Paragraph */}
+          <div className="mb-6 flex items-center gap-2">
+            <Checkbox
+              id="low-stock"
+              onChange={(e) => {
+                if (e.target.checked) {
+                  setFilteredProducts(
+                    products.filter((p) => parseFloat(p.product_stock) < 10)
+                  );
+                } else {
+                  setFilteredProducts(products);
+                }
+              }}
+            />
+            <Label htmlFor="low-stock" className="cursor-pointer">
+              Show only low stock items
+            </Label>
+          </div>
+
+          {/* Stock table */}
+          <InventoryTable productList={filteredProducts} />
+
+          {/* Bottom Buttons */}
+          <div className="flex justify-between fixed bottom-2 left-0 w-full px-6">
+            <Button
+              gradientDuoTone="purpleToBlue"
+              outline
+              onClick={() => setOpenModalUpcomming(true)}
+            >
+              Upcoming Stocks
+            </Button>
+            <div className="flex gap-4">
+              <Button
+                gradientDuoTone="purpleToBlue"
+                onClick={() => setOpenModalRequest(true)}
+              >
+                Request Product
+              </Button>
+              <Button
+                gradientDuoTone="greenToBlue"
+                outline
+                onClick={() => setOpenModalPending(true)}
+              >
+                Pending Request
+              </Button>
+            </div>
+          </div>
+
+          {/* Modals */}
+          <UpcommingStockInv
+            show={openModalUpcomming}
+            close={() => setOpenModalUpcomming(false)}
           />
-
-          {/* Category Selector */}
-          <Select
-            value={selectedType}
-            className="w-1/4"
-            onChange={(e) => {
-              setSelectedType(e.target.value);
-            }}
-          >
-            {productTypes.map((product, index) => (
-              <option value={product} key={index}>
-                {product}
-              </option>
-            ))}
-          </Select>
-
-          {/* Brand Selector */}
-          <Select
-            value={selectedBrand}
-            className="w-1/4"
-            onChange={(e) => {
-              setSelectedBrand(e.target.value);
-            }}
-          >
-            {brands.map((brand, index) => (
-              <option key={index} value={brand}>
-                {brand}
-              </option>
-            ))}
-          </Select>
+          <RequestProduct
+            show={openModalRequest}
+            close={() => setOpenModalRequest(false)}
+          />
+          <InventoryPendingRequest
+            show={openModalPending}
+            close={() => setOpenModalPending(false)}
+          />
         </div>
-      </div>
-
-      {/* Checkbox and Paragraph */}
-      <div className="mb-6 flex items-center gap-2">
-        <Checkbox
-          id="low-stock"
-          onChange={(e) => {
-            if (e.target.checked) {
-              setFilteredProducts(
-                products.filter((p) => parseFloat(p.product_stock) < 10)
-              );
-            } else {
-              setFilteredProducts(products);
-            }
-          }}
-        />
-        <Label htmlFor="low-stock" className="cursor-pointer">
-          Show only low stock items
-        </Label>
-      </div>
-
-      {/* Stock table */}
-      <InventoryTable productList={filteredProducts} />
-
-      {/* Bottom Buttons */}
-      <div className="flex justify-between fixed bottom-2 left-0 w-full px-6">
-        <Button
-          gradientDuoTone="purpleToBlue"
-          outline
-          onClick={() => setOpenModalUpcomming(true)}
-        >
-          Upcoming Stocks
-        </Button>
-        <div className="flex gap-4">
-          <Button
-            gradientDuoTone="purpleToBlue"
-            onClick={() => setOpenModalRequest(true)}
-          >
-            Request Product
-          </Button>
-          <Button
-            gradientDuoTone="greenToBlue"
-            outline
-            onClick={() => setOpenModalPending(true)}
-          >
-            Pending Request
-          </Button>
-        </div>
-      </div>
-
-      {/* Modals */}
-      <UpcommingStockInv
-        show={openModalUpcomming}
-        close={() => setOpenModalUpcomming(false)}
-      />
-      <RequestProduct
-        show={openModalRequest}
-        close={() => setOpenModalRequest(false)}
-      />
-      <InventoryPendingRequest
-        show={openModalPending}
-        close={() => setOpenModalPending(false)}
-      />
+      )}
     </div>
   );
 };
